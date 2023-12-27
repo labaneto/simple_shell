9b29fe3a95e1207b9bf44faa9f22e9a920bdeaf1
labaneto <labanmutie175@gmail.com>
2023-12-27 11:13:46 -0800

    sum.c

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Description - the sum of a and b
+ * @parameter - a and b
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 8, b = 7;
+	int Sum = (a + b);
+
+	printf("sum of a + b = %d\n", Sum);
+
+	return (0);
+}
